5ba1f5d302335285b40b58602aa7ab413dce7949
Galaxy Master <76506954+MasterofGalaxies@users.noreply.github.com>
2022-11-19 03:04:56 -0500

    Add --mat and link json-c

--- src/main.c
+++ src/main.c
@@ -8,6 +8,8 @@
 #define strncasecmp _strnicmp
 #endif
 
+#include <json.h>
+
 #include <assimp/scene.h>
 #include <assimp/cimport.h>
 
@@ -23,6 +25,7 @@ int main(int argc, char **argv) {
 	bool sm3das = false;
 	const char *input_path = NULL;
 	const char *output_path = NULL;
+	const char *mat_path = NULL;
 
 	bool parsing_args = true;
 
@@ -41,6 +44,8 @@ int main(int argc, char **argv) {
 			} else if (!strcmp(argv[i], "--sm3das")) {
 				output_bdl = true; // implies --bdl
 				sm3das = true;
+			} else if (!strcmp(argv[i], "--mat")) {
+				mat_path = argv[++i];
 			} else if (!strcmp(argv[i], "--")) {
 				parsing_args = false;
 			} else if (argv[i][0] == '-') {
@@ -76,6 +81,29 @@ int main(int argc, char **argv) {
 
 		if (output_len >= 4 && !strcasecmp(output_path + output_len - 4, ".bmd"))
 			output_bdl = false;
+	}
+
+	struct json_object *mat_obj = NULL;
+
+	if (mat_path) {
+		FILE *mat_fp = fopen(mat_path, "r");
+
+		if (!mat_fp) {
+			fprintf(stderr, "Failed to open %s: %s\n", mat_path, strerror(errno));
+			return 1;
+		}
+
+		fseek(mat_fp, 0, SEEK_END);
+		size_t mat_size = ftell(mat_fp);
+		fseek(mat_fp, 0, SEEK_SET);
+
+		char *mat = malloc(mat_size);
+
+		fread(mat, mat_size, 1, mat_fp);
+		fclose(mat_fp);
+
+		mat_obj = json_tokener_parse(mat);
+		free(mat);
 	}
 
 	const struct aiScene *model_data = aiImportFile(input_path, 0);
