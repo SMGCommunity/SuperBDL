979df745a1a2b1305ed92cb340c2c398fa64cdc5
Galaxy Master <76506954+MasterofGalaxies@users.noreply.github.com>
2022-11-19 22:48:59 -0500

    Continue matcmp

--- src/mat3.c
+++ src/mat3.c
@@ -1,5 +1,14 @@
 #include "mat3.h"
 
 bool matcmp(struct J3DMaterial* mat1, struct J3DMaterial* mat2) {
-    return false;
+    if (strcmp(mat1->Name, mat2->Name))
+        return false;
+
+    if (mat1->MaterialMode != mat2->MaterialMode)
+        return false;
+
+    if (mat1->EnableDithering != mat2->EnableDithering)
+        return false;
+
+    return true;
 }
