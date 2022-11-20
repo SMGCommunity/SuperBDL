41e5772a7bb1349e1cc4d2321bbf084c0b7abacd
Super Hackio <superhaxio@gmail.com>
2022-11-20 07:34:09 -0800

    [BMD] Change hardcoded strings to #defines
    For organization purposes and also because I wanna share the string with BMT

--- include/bmd.h
+++ include/bmd.h
@@ -2,8 +2,13 @@
 #define __BMD_H
 
 #include <stdbool.h>
+#include <stdio.h>
 
 #include <assimp/scene.h>
+
+#define MAGIC_J3D "J3D2"
+#define MAGIC_BDL "bdl4"
+#define MAGIC_BMD "bmd3"
 
 bool write_bmd(const struct aiScene *model_data, FILE *output_fp, bool write_bdl, bool sm3das);
 
--- src/bmd.c
+++ src/bmd.c
@@ -6,12 +6,12 @@
 #include "bmd.h"
 
 bool write_bmd(const struct aiScene *model_data, FILE *output_fp, bool write_bdl, bool sm3das) {
-	fwrite("J3D2", 4, 1, output_fp);
+	fwrite(MAGIC_J3D, 4, 1, output_fp);
 
 	if (write_bdl)
-		fwrite("bdl4", 5, 1, output_fp);
+		fwrite(MAGIC_BDL, 5, 1, output_fp);
 	else
-		fwrite("bmd3", 5, 1, output_fp);
+		fwrite(MAGIC_BMD, 5, 1, output_fp);
 
 	// true for success, false for error
 	return true;
