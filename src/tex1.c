b905dd1f3eb233d5a2aae9ee454c9c08ca97b0bf
Super Hackio <superhaxio@gmail.com>
2022-11-20 07:36:26 -0800

    [TEX1] Finished texcmp

--- src/tex1.c
+++ src/tex1.c
@@ -4,16 +4,10 @@ bool texcmp(struct JUTTexture* tex1, struct JUTTexture* tex2) {
 	if (strcmp(tex1->Name, tex2->Name))
 		return false;
 
-	if (tex1->AlphaSetting != tex2->AlphaSetting)
+	if (tex1->Width != tex2->Width)
 		return false;
 
-	if (tex1->ClampLODBias != tex2->ClampLODBias)
-		return false;
-
-	if (tex1->EnableMipmaps != tex2->EnableMipmaps)
-		return false;
-
-	if (tex1->MaxAnisotropy != tex2->MaxAnisotropy)
+	if (tex1->Height != tex2->Height)
 		return false;
 
 	if (tex1->Format != tex2->Format)
@@ -22,16 +16,31 @@ bool texcmp(struct JUTTexture* tex1, struct JUTTexture* tex2) {
 	if (tex1->PaletteFormat != tex2->PaletteFormat)
 		return false;
 
+	if (tex1->ImageCount != tex2->ImageCount)
+		return false;
+
 	if (tex1->WrapS != tex2->WrapS)
 		return false;
 
 	if (tex1->WrapT != tex2->WrapT)
 		return false;
 
+	if (tex1->AlphaSetting != tex2->AlphaSetting)
+		return false;
+
+	if (tex1->ClampLODBias != tex2->ClampLODBias)
+		return false;
+
+	if (tex1->MaxAnisotropy != tex2->MaxAnisotropy)
+		return false;
+
 	if (tex1->MagnificationFilter != tex2->MagnificationFilter)
 		return false;
 
 	if (tex1->MinificationFilter != tex2->MinificationFilter)
+		return false;
+
+	if (tex1->EnableMipmaps != tex2->EnableMipmaps)
 		return false;
 
 	if (tex1->MinLOD != tex2->MinLOD)
@@ -46,22 +55,11 @@ bool texcmp(struct JUTTexture* tex1, struct JUTTexture* tex2) {
 	if (tex1->EnableEdgeLOD != tex2->EnableEdgeLOD)
 		return false;
 
-	if (tex1->Width != tex2->Width)
+	if (tex1->ImageDataSize == tex2->ImageDataSize && !memcmp(tex1->ImageData, tex2->ImageData, tex1->ImageDataSize))
 		return false;
 
-	if (tex1->Height != tex2->Height)
+	if (tex1->PaletteDataSize == tex2->PaletteDataSize && !memcmp(tex1->PaletteData, tex2->PaletteData, tex1->PaletteDataSize))
 		return false;
-
-	if (tex1->ImageCount != tex2->ImageCount)
-		return false;
-
-#if 0
-	if (!memcmp(tex1->ImageData, tex2->ImageData, 0))
-		return false;
-
-	if (!memcmp(tex1->PaletteData, tex2->PaletteData, 0))
-		return false;
-#endif
 
 	return true;
 }
